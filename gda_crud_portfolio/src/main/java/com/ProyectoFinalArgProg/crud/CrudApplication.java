d4682d48b71f19c7244d4d30eb03987e4cfdd0c4
Guillermo <guillermo.arena@gmail.com>
2022-07-19 20:01:00 -0300

    CRUD

---
+++ gda_crud_portfolio/src/main/java/com/ProyectoFinalArgProg/crud/CrudApplication.java
@@ -0,0 +1,14 @@
+//CRUD__GDA
+package com.ProyectoFinalArgProg.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
